6e56b6be552f403c1c1657634ec57c5f3a08b4b8
suryatejaj97 <suryatejaj97@gmail.com>
2021-07-19 20:38:37 +0530

    edamam api added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import axios from "axios"
 
 const App = () => {
-    return (
-        <div>
-        <h1>Food Recipe App</h1>      
-        </div>
-    )
-}
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
 
-export default App
+  const url = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const getData = async () => {
+    const result = await axios.get(url)
+    console.log(result);
+  }
+  return (
+    <div className="App">
+      <h1 onClick={getData}>Food Recipe App</h1>
+    </div>
+  );
+};
+
+export default App;
