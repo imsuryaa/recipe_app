7abd350906054b60900d012745053b3fae2b84e7
suryatejaj97 <suryatejaj97@gmail.com>
2021-07-20 23:38:14 +0530

    alerts added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
 
 const App = () => {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
 
   const APP_ID = "4e9f05eb";
   const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
   const getData = async () => {
-    const result = await axios.get(url);
-    setRecipes(result.data.hits)
-    console.log(result);
-    setQuery("");
+    if (query !== "") {
+      const result = await axios.get(url);
+      if(!result.data.more){
+          return setAlert("No food with such name")
+      }
+      setRecipes(result.data.hits);
+      console.log(result);
+      setAlert("")
+      setQuery("");
+    } else {
+        setAlert("Please fill the form")
+    }
   };
   const onChange = (e) => {
     setQuery(e.target.value);
@@ -29,6 +39,7 @@ const App = () => {
     <div className="App">
       <h1>Food Recipe App</h1>
       <form className="search-form" onSubmit={onSubmit}>
+        {alert !== "" && <Alert alert={alert}/>}
         <input
           type="text"
           placeholder="Search Food"
@@ -39,9 +50,8 @@ const App = () => {
         <input type="submit" value="search" />
       </form>
       <div className="recipes">
-        {recipes !== [] && recipes.map(recipe => (
-            <Recipe key={uuidv4} recipe={recipe} />
-        ))}
+        {recipes !== [] &&
+          recipes.map((recipe) => <Recipe key={uuidv4} recipe={recipe} />)}
       </div>
     </div>
   );
